09ab939748d6b7f7e299a381d32d88065a4bccba
Kaloyan Simitchiyski <kaloyan.sim@gmail.com>
2018-10-15 21:33:56 +0300

    Adapt for RabbitMQ on SAP CP

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 "use strict";
 
-const url = require("url");
 const http = require("http");
 const httpProxy = require("http-proxy");
 const winston = require("winston");
 
 const vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-const serviceUrl = url.parse(vcapServices.rabbitmqent[0].credentials.protocols.management.uri);
+const credentials = vcapServices.rabbitmq[0].credentials;
 const address = {
-  host: serviceUrl.hostname,
-  port: serviceUrl.port,
+  host: credentials.hostname,
+  port: credentials.ports["15672/tcp"]
 };
 
 const proxy = httpProxy.createServer();
